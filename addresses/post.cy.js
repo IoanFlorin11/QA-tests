cba9273d01e399cf43a51abcebfbddf6fdebac96
IoanFlorin11 <ioan.florinh@icloud.com>
2022-09-02 09:48:36 +0300

    NATCOM endpoint- negative cases included

--- addresses/post.cy.js
+++ addresses/post.cy.js
@@ -8,20 +8,45 @@ describe('POST address', () => {
             headers: {DemandPartnerId: '3fad6e81-d602-46b2-8c50-a7515f6a0b4e'},
             body:
             {
-                "addressLine1": "string441",
-                "addressLine2": "string5",
-                "addressLine3": "string6",
-                "city": "string7",
-                "stateCode": "string9",
-                "countryCode": "string33",
-                "zipCode": "string22",
-                "contactName": "string66",
-                "companyName": "string88",
-                "phone": "string99",
+                "addressLine1": "automationTest12",
+                "addressLine2": "string25",
+                "addressLine3": "string26",
+                "city": "string27",
+                "stateCode": "string29",
+                "countryCode": "string332",
+                "zipCode": "string222",
+                "contactName": "string662",
+                "companyName": "string882",
+                "phone": "string929",
                 "email": "string00"
             }
         })
         expect(response.status).to.eq(201)
-        // expect(response.body).has.property('city', 'string7')
+
   })
-  })
+
+  it('Negative case- Neither of  addressLine1, city, zipCode, stateCode, contactName, countryCode can  null; phone can not be null or empty', async () => {
+    const response = await cy.request({
+        method: 'POST',
+        url: demandPartnerPath,
+        failOnStatusCode: false,
+        headers: {DemandPartnerId: '3fad6e81-d602-46b2-8c50-a7515f6a0b4e'},
+        body:
+        {
+            // "addressLine1": "automationTest",
+            "addressLine2": "string35",
+            "addressLine3": "string63",
+            // "city": "string73",
+            // "stateCode": "string93",
+            // "countryCode": "string333",
+            // "zipCode": "string232",
+            // "contactName": "string663",
+            // "companyName": "string883",
+            // "phone": "string993",
+            // "email": "string003"
+        }
+    })
+    expect(response.status).to.eq(400)
+})
+})
+
